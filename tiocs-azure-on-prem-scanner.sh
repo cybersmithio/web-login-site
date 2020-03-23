b6b2890791210f5588beb01fe8e1fb82a7e76aaf
jamessmith <james@cybersmith.io>
2020-03-22 21:39:01 -0400

    Added example of Jenkins pipeline

--- tiocs-azure-on-prem-scanner.sh
+++ tiocs-azure-on-prem-scanner.sh
@@ -25,7 +25,14 @@ docker pull tenableio-docker-consec-local.jfrog.io/cs-scanner:latest
 
 echo "Start of on-prem analysis"
 set -x
+
+
+
+
 docker save $IMAGEREPOSITORY:$BUILD_BUILDID | docker run -e DEBUG_MODE=true -e TENABLE_ACCESS_KEY=$TIOACCESSKEY -e TENABLE_SECRET_KEY=$TIOSECRETKEY -e IMPORT_REPO_NAME=$IMAGEREPOSITORY -i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest inspect-image $IMAGEREPOSITORY:$BUILD_BUILDID
+
+
+
 if [ $? != 0 ]; then
   echo "Error analyzing container image"
   exit 1
