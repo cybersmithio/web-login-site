d9bcda605a09851cc8246d28c985434e3da6814c
jamessmith <james@cybersmith.io>
2020-05-21 14:13:14 -0400

    Updated

--- tiocs-azure-on-prem-scanner.sh
+++ tiocs-azure-on-prem-scanner.sh
@@ -8,7 +8,7 @@ TIOSECRETKEY=$2
 TIOJFROGPASS=$3
 
 IMAGEREPOSITORY=$BUILD_REPOSITORY_ID
-IMAGEREPOSITORY=tenabledemoacr.azurecr.io/web-login-site:latest
+IMAGEREPOSITORY=tenabledemoacr.azurecr.io/web-login-site
 echo "Checking $IMAGEREPOSITORY:$BUILD_BUILDID and analyzing results on-premise then reporting into cloud.tenable.com repo $IMAGEREPOSITORY"
 echo "Tenable.io Access Key: $TIOACCESSKEY"
 echo ""
