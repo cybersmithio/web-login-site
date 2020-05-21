d65a47cd4b95065e5c185794c3bdca3748024cf9
jamessmith <james@cybersmith.io>
2020-05-21 16:48:31 -0400

    Updated build script to use the new CHECK_POLICY flag for the Tenable CS scanner

--- tiocs-azure-on-prem-scanner.sh
+++ tiocs-azure-on-prem-scanner.sh
@@ -24,44 +24,5 @@ docker pull tenableio-docker-consec-local.jfrog.io/cs-scanner:latest
 #docker images
 
 echo "Start of on-prem analysis"
-set -x
+docker save $IMAGEREPOSITORY:$BUILD_BUILDID | docker run -e CHECK_POLICY=true -e DEBUG_MODE=true -e TENABLE_ACCESS_KEY=$TIOACCESSKEY -e TENABLE_SECRET_KEY=$TIOSECRETKEY -e IMPORT_REPO_NAME=$IMAGEREPOSITORY -i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest inspect-image $IMAGEREPOSITORY:$BUILD_BUILDID
 
-
-#IMAGEREPOSITORY=$BUILD_REPOSITORY_ID
-#IMAGEREPOSITORY=$BUILD_DEFINITIONNAME
-#IMAGEREPOSITORY=tenabledemoacr.azurecr.io/web-login-site
-#IMAGEREPOSITORY=tenabledemoacr.azurecr.io/web-login-site
-
-docker images
-docker ps -a
-docker save $IMAGEREPOSITORY:$BUILD_BUILDID | docker run -e DEBUG_MODE=true -e TENABLE_ACCESS_KEY=$TIOACCESSKEY -e TENABLE_SECRET_KEY=$TIOSECRETKEY -e IMPORT_REPO_NAME=$IMAGEREPOSITORY -i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest inspect-image $IMAGEREPOSITORY:$BUILD_BUILDID
-
-#docker save $BUILD_DEFINITIONNAME:$BUILD_BUILDID | docker run -e DEBUG_MODE=true -e TENABLE_ACCESS_KEY=$TIOACCESSKEY -e TENABLE_SECRET_KEY=$TIOSECRETKEY -e IMPORT_REPO_NAME=$BUILD_DEFINITIONNAME -i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest inspect-image $IMAGEREPOSITORY:$BUILD_BUILDID
-
-
-if [ $? != 0 ]; then
-  echo "Error analyzing container image"
-  exit 1
-fi
-set +x
-echo "End of on-prem analysis"
-
-echo "Download report on image"
-while [ 1 -eq 1 ]; do
-  RESP=`curl -s --request GET --url "https://cloud.tenable.com/container-security/api/v1/compliancebyname?image=$IMAGEREPOSITORY&repo=$IMAGEREPOSITORY&tag=$BUILD_BUILDID" \
-  --header 'accept: application/json' --header "x-apikeys: accessKey=$TIOACCESSKEY;secretKey=$TIOSECRETKEY" \
-  | sed -n 's/.*\"status\":\"\([^\"]*\)\".*/\1/p'`
-  echo "Report status: $RESP"
-  if [ "x$RESP" = "xpass" ] ; then
-    echo "Container marked as PASSED by policy rules"
-    echo "Retagging for container registry"
-    docker tag $IMAGEREPOSITORY:$BUILD_BUILDID $CONTAINERREGISTRY/$IMAGEREPOSITORY:$BUILD_BUILDID
-    exit 0
-  fi
-  if [ "x$RESP" = "xfail" ] ; then
-    echo "Container marked as FAILED by policy rules"
-    exit 1
-  fi
-  echo "Waiting 30 seconds before checking again for report"
-  sleep 30
-done
