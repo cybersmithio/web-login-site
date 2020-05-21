b8d4d267372647001b1f9b737bce88c6a1bff7fb
jamessmith <james@cybersmith.io>
2020-05-21 14:32:47 -0400

    Updated

--- tiocs-azure-on-prem-scanner.sh
+++ tiocs-azure-on-prem-scanner.sh
@@ -32,7 +32,8 @@ set -x
 #IMAGEREPOSITORY=tenabledemoacr.azurecr.io/web-login-site
 #IMAGEREPOSITORY=tenabledemoacr.azurecr.io/web-login-site
 
-
+docker images
+docker ps -a
 docker save $BUILD_REPOSITORY_ID:$BUILD_BUILDID | docker run -e DEBUG_MODE=true -e TENABLE_ACCESS_KEY=$TIOACCESSKEY -e TENABLE_SECRET_KEY=$TIOSECRETKEY -e IMPORT_REPO_NAME=$BUILD_REPOSITORY_ID -i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest inspect-image $IMAGEREPOSITORY:$BUILD_BUILDID
 
 docker save $BUILD_DEFINITIONNAME:$BUILD_BUILDID | docker run -e DEBUG_MODE=true -e TENABLE_ACCESS_KEY=$TIOACCESSKEY -e TENABLE_SECRET_KEY=$TIOSECRETKEY -e IMPORT_REPO_NAME=$BUILD_DEFINITIONNAME -i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest inspect-image $IMAGEREPOSITORY:$BUILD_BUILDID
