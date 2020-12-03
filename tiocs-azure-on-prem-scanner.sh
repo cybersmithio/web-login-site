daacf512f305dc5ef4c6183d09f2e77f7679fd6d
jamessmith <james@cybersmith.io>
2020-12-03 10:08:00 -0500

    Adding YML for Gitlabs

--- tiocs-azure-on-prem-scanner.sh
+++ tiocs-azure-on-prem-scanner.sh
@@ -1,46 +1,15 @@
 #!/bin/sh
-# This script excepts 3 command-line arguments in this particular order: Tenable.io Access Key, Tenable.io Secret Key, Tenable.io JFrog pubread password
-#This script expects the following environment variables to be set:
-# BUILD_BUILDID - This is set by Azure if you have called this script from a Docker Build and Push step
-# IMAGEREPOSITORY - This is set by Azure if you have called this script from a Docker Build and Push step
 TIOACCESSKEY=$1
 TIOSECRETKEY=$2
 TIOJFROGPASS=$3
 
-### Uncomment the lines below for debugging
-#echo "Checking $IMAGEREPOSITORY:$BUILD_BUILDID and analyzing results on-premise then reporting into cloud.tenable.com repo $IMAGEREPOSITORY"
-#echo "Tenable.io Access Key: $TIOACCESSKEY"
-#echo ""
-#echo "Variables list:"
-#set
-#echo ""
-#echo "Download Tenable.io on-prem scanner"
-
 docker login --username pubread --password $TIOJFROGPASS tenableio-docker-consec-local.jfrog.io
 docker pull tenableio-docker-consec-local.jfrog.io/cs-scanner:latest
 
-### Uncomment line below for debugging
-#docker images
-#echo "Start of on-prem analysis"
+docker save $CONTAINERREGISTRY/$IMAGEREPOSITORY:$BUILD_BUILDID | docker run -e CHECK_POLICY=true \
+-e DEBUG_MODE=true \
+-e TENABLE_ACCESS_KEY=$TIOACCESSKEY \
+-e TENABLE_SECRET_KEY=$TIOSECRETKEY \
+-e IMPORT_REPO_NAME=$IMAGEREPOSITORY \
+-i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest inspect-image $IMAGEREPOSITORY:$BUILD_BUILDID
 
-docker save $CONTAINERREGISTRY/$IMAGEREPOSITORY:$BUILD_BUILDID | docker run -e CHECK_POLICY=true -e DEBUG_MODE=true -e TENABLE_ACCESS_KEY=$TIOACCESSKEY -e TENABLE_SECRET_KEY=$TIOSECRETKEY -e IMPORT_REPO_NAME=$IMAGEREPOSITORY -i tenableio-docker-consec-local.jfrog.io/cs-scanner:latest inspect-image $IMAGEREPOSITORY:$BUILD_BUILDID
-
-echo "Download report on image"
-while [ 1 -eq 1 ]; do
-  RESP=`curl -s --request GET --url "https://cloud.tenable.com/container-security/api/v1/compliancebyname?image=$IMAGEREPOSITORY&repo=$IMAGEREPOSITORY&tag=$BUILD_BUILDID" \
-  --header 'accept: application/json' --header "x-apikeys: accessKey=$TIOACCESSKEY;secretKey=$TIOSECRETKEY" \
-  | sed -n 's/.*\"status\":\"\([^\"]*\)\".*/\1/p'`
-  echo "Report status: $RESP"
-  if [ "x$RESP" = "xpass" ] ; then
-    echo "Container marked as PASSED by policy rules"
-    echo "Retagging for container registry"
-    docker tag $IMAGEREPOSITORY:$BUILD_BUILDID $CONTAINERREGISTRY/$IMAGEREPOSITORY:$BUILD_BUILDID
-    exit 0
-  fi
-  if [ "x$RESP" = "xfail" ] ; then
-    echo "Container marked as FAILED by policy rules"
-    exit 1
-  fi
-  echo "Waiting 30 seconds before checking again for report"
-  sleep 30
-done
